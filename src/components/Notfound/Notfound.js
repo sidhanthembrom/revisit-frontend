9ed16e6039948cee979e0e0380b54aaba6c89b1d
sidhanthembrom <hembrom.sidhant@gmail.com>
2025-04-13 12:40:20 +0530

    rectified notfound page

--- src/components/Notfound/Notfound.js
+++ src/components/Notfound/Notfound.js
@@ -3,7 +3,7 @@ import "./Notfound.css";
 function Notfound() {
   return (
     <div className="notfound-bg">
-      <div className="text-container">
+      <div className="notfound-text-container">
         <h1>Looks like you're lost in space</h1>
       </div>
     </div>
